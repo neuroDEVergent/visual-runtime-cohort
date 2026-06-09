bedb81e1e01bb7912839b617ca057a5680e99ee0
neuroDEVergent <braticpetar44@proton.me>
2026-06-09 15:41:57 +0200

    deleted unnecessary key members in the input struct

--- core/include/input.h
+++ core/include/input.h
@@ -18,11 +18,6 @@ struct Input {
   double        SCROLL_Y = 1.0f;
   unsigned int  SCREEN_WIDTH;
   unsigned int  SCREEN_HEIGHT;
-  bool               ONE;
-  bool               TWO;
-  bool             THREE;
-  bool              FOUR;
-  bool              FIVE;
   bool       LCLICK_DOWN;  // Button being held down
   bool       RCLICK_DOWN;
   bool    LCLICK_PRESSED;  // Signal that button has been clicked (signaled once per frame)
